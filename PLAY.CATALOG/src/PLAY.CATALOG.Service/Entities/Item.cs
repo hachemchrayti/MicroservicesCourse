44e23890a194105478f05464121dc426cb219943
hachemchrayti <hachem.chrayti@outlook.fr>
2021-11-29 20:19:31 +0100

    Implementing MongoDB repository for CUD operations

---
+++ PLAY.CATALOG/src/PLAY.CATALOG.Service/Entities/Item.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PLAY.CATALOG.Service.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTimeOffset CreationDate { get; set; }
+
+    }
+}
---
+++ PLAY.CATALOG/src/PLAY.CATALOG.Service/Repositories/ItemsRepository.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using PLAY.CATALOG.Service.Entities;
+
+namespace PLAY.CATALOG.Service.Repositories
+{
+    public class ItemsRepository
+    {
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> dbCollection;
+
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public ItemsRepository()
+        {
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var database = mongoClient.GetDatabase("Catalog");
+            dbCollection = database.GetCollection<Item>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<Item>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(e => e.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+        public async Task CreateAsync(Item entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentException(nameof(entity));
+            }
+
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+
+
+        public async Task UpdateAsync(Item entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentException(nameof(entity));
+            }
+            FilterDefinition<Item> filter = filterBuilder.Eq(e => e.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+        public async Task Removesync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(e => e.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
+        }
+
+    }
+}
